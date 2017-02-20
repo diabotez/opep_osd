5b42f27505c770830027bef453a59019b5d4a583
diabotez <dia.botez@gmail.com>
2017-02-20 17:59:17 +0100

    updating the doctor table and their operation room planing. Made a main method for all program. Added the option to oppen the right language version after selecting the language from the welcome window.

--- operationPlanning/src/operationplanning/OperationPlanning.java
+++ operationPlanning/src/operationplanning/OperationPlanning.java
@@ -5,33 +5,25 @@
  */
 package operationplanning;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javax.swing.JApplet;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
 
 /**
  *
  * @author user
  */
-public class OperationPlanning extends JApplet {
-    
-    private static final int JFXPANEL_WIDTH_INT = 300;
-    private static final int JFXPANEL_HEIGHT_INT = 250;
-    private static JFXPanel fxContainer;
+public
+    class OperationPlanning extends JApplet
+{
 
-    /**
-     * @param args the command line arguments
-     */
+    private static final
+        int JFXPANEL_WIDTH_INT = 300;
+    private static final
+        int JFXPANEL_HEIGHT_INT = 250;
+    private static
+        JFXPanel fxContainer;
+    
+/*
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             
@@ -73,7 +65,7 @@ public class OperationPlanning extends JApplet {
             }
         });
     }
-    
+        
     private void createScene() {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
@@ -88,5 +80,50 @@ public class OperationPlanning extends JApplet {
         root.getChildren().add(btn);
         fxContainer.setScene(new Scene(root));
     }
+*/
     
+    /**
+     * @param args the command line arguments
+     */
+    public static
+        void main(String args[])
+    {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            javax.swing.UIManager.LookAndFeelInfo[] installedLookAndFeels = javax.swing.UIManager.getInstalledLookAndFeels();
+            for (int idx = 0; idx < installedLookAndFeels.length; idx++) {
+                if ("Nimbus".equals(installedLookAndFeels[idx].getName())) {
+                    javax.swing.UIManager.setLookAndFeel(installedLookAndFeels[idx].getClassName());
+                    break;
+                }
+            }
+        }
+        catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(WelcomeForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(WelcomeForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(WelcomeForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(WelcomeForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable()
+        {
+            public
+                void run()
+            {
+                new WelcomeForm().setVisible(true);
+            }
+        });
+    }
 }
